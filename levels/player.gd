af315675202a0d01a9db70c01347bc81546f0a08
Bohdan <bomtvv@gmail.com>
2021-02-14 15:38:29 +0200

    player movement

--- levels/player.gd
+++ levels/player.gd
@@ -1,4 +1,24 @@
 extends RigidBody
+
+export var SPEED := 10.0
+export(NodePath) onready var camera = get_node(camera) as Camera
+
+func _physics_process(delta):
+	var torque := Vector3(0, 0, 0)
 	
-func _ready():
-	pass
+	if Input.is_action_pressed("move_forward"):
+		torque.z = -1
+	elif Input.is_action_pressed("move_backwards"):
+		torque.z = 1
+	
+	if Input.is_action_pressed("move_right"):
+		torque.x = -1
+	elif Input.is_action_pressed("move_left"):
+		torque.x = 1
+	
+	torque *= SPEED
+	
+	torque.z *= camera.get_global_transform().basis.z
+	torque.x *= camera.get_global_transform().basis.x
+	
+	add_torque(torque)
